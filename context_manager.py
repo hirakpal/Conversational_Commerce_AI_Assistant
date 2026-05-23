95058615936d7847b3a2263693a2ad3f919d5118
hirakpal <60995278+hirakpal@users.noreply.github.com>
2026-05-24 00:46:30 +0530

    Change default Redis port from 6379 to 12621

--- context_manager.py
+++ context_manager.py
@@ -6,7 +6,7 @@ class ContextManager:
     def __init__(self):
         # Read variables configured via Streamlit Secrets
         redis_host = os.environ.get("REDIS_HOST", "localhost")
-        redis_port = int(os.environ.get("REDIS_PORT", 6379))
+        redis_port = int(os.environ.get("REDIS_PORT", 12621))
         redis_password = os.environ.get("REDIS_PASSWORD", None)
 
         self.db = redis.Redis(
